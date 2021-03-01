5a61ada53b1a2ec63972937065dde4470d30dd4c
Randy Eckenrode <randy@largeandhighquality.com>
2021-02-28 21:56:44 -0500

    Don’t consider adjacent areas as overlapping

--- scripts/speed-provider.js
+++ scripts/speed-provider.js
@@ -25,10 +25,10 @@ Hooks.once("dragRuler.ready", (SpeedProvider) => {
   }
 
   function contains(rect1, rect2) {
-    return !(rect1[0] > rect2[2]
-      || rect1[2] < rect2[0]
-      || rect1[1] > rect2[3]
-      || rect1[3] < rect2[1]);
+    return !(rect1[0] >= rect2[2]
+      || rect1[2] <= rect2[0]
+      || rect1[1] >= rect2[3]
+      || rect1[3] <= rect2[1]);
   }
 
   function getCenters(token, jitter) {
